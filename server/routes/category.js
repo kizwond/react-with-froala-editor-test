1c629c1326f8c15576ee6e32b93222496d220aa9
kizwond <kizwond@gmail.com>
2020-09-23 03:27:16 +0900

    delete category routes add contents_quantity part

--- server/routes/category.js
+++ server/routes/category.js
@@ -93,6 +93,7 @@ router.post('/delete-category', async (req, res) => {
   const willAddHere = await BookTitle.findOne({user_id: req.body.userId, category : req.body.moveTo}).sort({ 'list_order' : -1 }).exec(); 
 
   if (req.body.moveTo === '') {
+    //카테고리와 함께 포함된 책들도 삭제, 즐겨찾기 순서 재조정
     const books = await BookTitle.deleteMany({user_id : req.body.userId, category:currentOrder.category_name}).exec() 
     const likes = await BookTitle.find({user_id : req.body.userId, like: 'true' }).sort({like_order : 1}).exec()
     .then((result) => {
@@ -105,6 +106,12 @@ router.post('/delete-category', async (req, res) => {
     });
     
   } else {
+    //카테고리 이동후 삭제시, 이동 카테고리에 추가된 책 수만큼 Add contents_quantity 
+    const booksToMove = await BookTitle.find({user_id : req.body.userId, category:currentOrder.category_name})
+    const categoryTo = await Category.findOne({user_id : req.body.userId, category_name : req.body.moveTo})
+    const addQuantity = await Category.findOneAndUpdate({user_id : req.body.userId, category_name : req.body.moveTo},{contents_quantity:categoryTo.contents_quantity + booksToMove.length})
+
+    //카테고리 이동 후, 이동한 카테고리의 리스트 순서 뒤부터 넘버링
     const books = await BookTitle.find({user_id : req.body.userId, category:currentOrder.category_name}).exec()
     .then((result) => {
       {result.map((value, index) => {
@@ -114,18 +121,24 @@ router.post('/delete-category', async (req, res) => {
     .catch((err) => {
       console.error(err);
     });
+    
+    
   }
-  const others = await Category.find({user_id: req.body.userId, category_order : {$gt : currentOrder.category_order}}).exec()
+  // 카테고리 삭제
+  let doc = await Category.deleteOne({_id: req.body.categoryId});
+
+  //삭제된 카테고리 이후 순서 카테고리들 순서 재조정, 카테고리 순서 소트 후 인덱스 + 1로 재조정
+  const others = await Category.find({user_id: req.body.userId}).sort({category_order: 1 }).exec()
   .then((result) => {
     {result.map((value, index) => {
-      return Category.updateMany({ _id: value._id }, { category_order: value.category_order - 1 }).exec();
+      return Category.updateMany({ _id: value._id }, { category_order: index + 1 }).exec();
     })}
   })
   .catch((err) => {
     console.error(err);
   });
 
-  let doc = await Category.deleteOne({_id: req.body.categoryId});
+ 
   const bookTitle = await BookTitle.find({user_id: req.body.userId}).sort({ 'category' : 1,'list_order': 1 }).exec();
   const likeTitle = await BookTitle.find({user_id: req.body.userId}).sort({ 'like_order': 1 }).exec();
   const category = await Category.find({user_id: req.body.userId}).sort({ 'category_order': 1 }).exec();
