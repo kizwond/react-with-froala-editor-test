80f1cece0baf1434620514d9bcc96b0dfb2d9a44
kizwond <kizwond@gmail.com>
2020-09-23 03:40:41 +0900

    delete category and move books select option state value handle

--- client/src/components/views/Write/DeleteCategory.js
+++ client/src/components/views/Write/DeleteCategory.js
@@ -20,10 +20,16 @@ class DeleteCategory extends Component {
       moveTo : value
     })
   }
+  handleChangeInside = (value) => {
+    console.log(value)
+    this.setState({
+      moveTo : ''
+    })
+  }
   deleteThis = (value) => {
     this.props.categoryDeleteHandler({value, moveTo:this.state.moveTo})
   }
-  showPromiseConfirm = (category_id, event) => {
+  showPromiseConfirm = (category_id, event, handleChangeInside) => {
     console.log(this)
     if(this.props.categoryTotal.length >= 0) {
       var optionList = this.props.categoryTotal.map((category)=>(
@@ -38,20 +44,25 @@ class DeleteCategory extends Component {
       cancelText: '취소',
       icon: <ExclamationCircleOutlined />,
       content: [<WarningTwoTone twoToneColor="red"/>,<span style={{fontSize:"12px",fontWeight:"700"}}> 삭제시 <span style={{color:"red"}}>영구 삭제</span> 되오니 신중히 결정 하십시오.</span>,
-                                                    <br/>,<span style={{fontSize:"11px",fontWeight:"700"}}>- 다른 카테고리를 선택하지 않을시 포함된 책 또한 함께 삭제됩니다.</span>,<br/>,<span style={{fontSize:"11px",fontWeight:"700"}}>- 구매한 책은 미지정 카테고리로 이동되며, 본인이 직접 만든 책은 영구삭제 됩니다.</span>,
-                                                    <br/>,<div className="selection_move_down" style={{fontSize:"11px",fontWeight:"700"}}><span>포함된 책을</span> <Select defaultValue="카테고리 선택" size="small" style={{ fontSize:'11px', width: 160 }} onChange={this.handleChange}>
+                                                    <br/>,<span style={{fontSize:"11px",fontWeight:"700"}}>- 다른 카테고리를 선택하지 않을시 포함된 책 또한 함께 삭제됩니다.</span>,<br/>,
+                                                    <span style={{fontSize:"11px",fontWeight:"700"}}>- 구매한 책은 미지정 카테고리로 이동되며, 본인이 직접 만든 책은 영구삭제 됩니다.</span>,
+                                                    <br/>,<div className="selection_move_down" style={{fontSize:"11px",fontWeight:"700"}}><span>포함된 책을</span> <Select defaultValue="카테고리 선택" 
+                                                    size="small" style={{ fontSize:'11px', width: 160 }} onChange={this.handleChange}>
+                                                    <Option key={'default'} value=''>카테고리 선택</Option>
                                                     {optionList}
                                                   </Select> <span>로 이동 후</span></div>],
       onOk() {  
         event({categoryId:category_id._id})
       },
-      onCancel() {},
+      onCancel() {
+        handleChangeInside()
+      },
     });
   }
   render() { 
     return ( 
       <Space>
-        <DeleteOutlined onClick={()=>this.showPromiseConfirm(this.state.category, this.deleteThis)} style={{fontSize:'14px'}} />
+        <DeleteOutlined onClick={()=>this.showPromiseConfirm(this.state.category, this.deleteThis, this.handleChangeInside)} style={{fontSize:'14px'}} />
       </Space>
     );
   }
