5bae9dd07646095f95742880b879e9de7315160c
kizwond <kizwond@gmail.com>
2020-09-22 09:10:42 +0900

    popover form & add new category, onclick event after submit or cancel fixed

--- client/src/components/views/Write/CategorySettingModal.js
+++ client/src/components/views/Write/CategorySettingModal.js
@@ -1,107 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { Modal, Popover,Form, Input, Button, Space  } from 'antd';
 import './CategorySettingModal.css'
 import { SettingOutlined, PlusOutlined,DeleteOutlined,ArrowUpOutlined,ArrowDownOutlined,EditOutlined} from '@ant-design/icons';
 import ChangeCategoryName from './ChangeCategoryName'
 
-      
+// const [form] = Form.useForm();
 
-const CategoryList = (props) =>{
-  const [form] = Form.useForm();
-  const [inputArea, SetInputArea] = useState(false)
-  
+class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      inputArea : false,
+      newInput : false
+     };
+  }  
 
-  const onFinish = value => {
-    props.addCategory({value, prevCategoryId:props.category._id})
-    // props.onClick()
+  onFinish = value => {
+    this.props.addCategory({value, prevCategoryId:this.props.category._id})
+    this.newInputVisible()
   };
-  const inputAreaVisible = () =>{
-    SetInputArea({
-      inputArea: !inputArea
-    });
-    console.log(inputArea)
+
+  inputAreaVisible = () =>{
+    this.setState(state => ({
+      inputArea: !state.inputArea
+    }));
   }
-  
+  newInputVisible = () =>{
+    this.setState(state => ({
+      newInput: !state.newInput
+    }));
+  }
+  render() {
+    
+    const text = <span>새로운 카테고리 이름을 입력해 주세요.</span>;
+    const content = (
+      <Form
+          layout={'inline'}
+          size="small"
+          onFinish={this.onFinish}
+          className="change_book_title_input_form"
+        >
+          <Space>
+          <Form.Item name={['newCategory']} rules={[{ required: true }]} >
+            <Input placeholder='' />
+          </Form.Item>
+          <Form.Item className="change_book_title_buttons">
+            <Button type="primary" htmlType="submit">완료</Button>
+            <Button type="primary" onClick={this.newInputVisible}>취소</Button>
+          </Form.Item>
+          </Space>
+        </Form>
+    );
 
-
-  const text = <span>새로운 카테고리 이름을 입력해 주세요.</span>;
-  const content = (
-    <Form
-        layout={'inline'}
-        form={form}
-        size="small"
-        onFinish={onFinish}
-        className="change_book_title_input_form"
-      >
-        <Space>
-        <Form.Item name={['newCategory']} rules={[{ required: true }]} >
-          <Input placeholder='' />
-        </Form.Item>
-        <Form.Item className="change_book_title_buttons">
-          <Button type="primary" htmlType="submit">완료</Button>
-          <Button type="primary" onClick={props.onClick}>취소</Button>
-        </Form.Item>
-        </Space>
-      </Form>
-  );
-
-  return(
+    return(
         <div className="category_setting_content">
           <ul>
             <li>
-              <Popover placement="rightTop" editCategory title={text} content={content} trigger="click">
-                <PlusOutlined style={{fontSize:'14px'}} />
+              <Popover placement="rightTop" editCategory title={text} visible={this.state.newInput} content={content} trigger="click">
+                <PlusOutlined onClick={this.newInputVisible} style={{fontSize:'14px'}} />
               </Popover>
             </li>
-            <li>{inputArea ? <ChangeCategoryName visible={inputArea} onClick={inputAreaVisible} category={props.category} changeCategoryHandler={props.changeCategoryHandler}/> : <>{props.category.category_name}/순서:{props.category.category_order} </>}</li>
+            <li>{this.state.inputArea ? <ChangeCategoryName vi={this.state.inputArea} inputAreaVisible={this.inputAreaVisible} category={this.props.category} changeCategoryHandler={this.props.changeCategoryHandler}/> : <>{this.props.category.category_name}/순서:{this.props.category.category_order} </>}</li>
             <li>
-              {props.category.category_name === '미지정' ? '' :<EditOutlined onClick={inputAreaVisible} style={{fontSize:'14px'}}/>}
+              {this.props.category.category_name === '미지정' ? '' :<EditOutlined onClick={this.inputAreaVisible} style={{fontSize:'14px'}}/>}
             </li>
             <li>
-              {props.category.category_name === '미지정' ? '' : <><ArrowUpOutlined style={{fontSize:'14px'}}/>
+              {this.props.category.category_name === '미지정' ? '' : <><ArrowUpOutlined style={{fontSize:'14px'}}/>
                                                             <ArrowDownOutlined style={{fontSize:'14px'}}/></>}
             </li>
-            <li>{props.category.category_name === '미지정' ? '' :<DeleteOutlined style={{fontSize:'14px'}}/>}</li>
-            <li>{props.category.contents_quantity}</li>
+            <li>{this.props.category.category_name === '미지정' ? '' :<DeleteOutlined style={{fontSize:'14px'}}/>}</li>
+            <li>{this.props.category.contents_quantity}</li>
             <li>한국사1, 한국사2, 한국사3, 한국사4, 한국사5, 한국사6, 한국사7, 한국사8</li>
           </ul>
         </div>
-  )
+    )
+  }
 }
 
 
-const CategoryModal = (props) => {
-  const [visible, setVisible] = useState(false);
-  const categoryList = props.category.map((category)=>(
-    <CategoryList changeCategoryHandler={props.changeCategoryHandler} addCategory={props.addCategory} key={category._id} category={category}/>
-  ))
-  
-  return (
-    <>
-      <SettingOutlined  onClick={() => setVisible(true)} style={{fontSize:'14px'}}/>
-      <Modal
-        title={[<SettingOutlined  onClick={() => setVisible(true)} style={{fontSize:'14px'}}/>,<span style={{fontSize:"12px"}}> 카테고리 설정</span>]}
-        visible={visible}
-        onCancel={() => setVisible(false)}
-        width={1000}
-        footer={null}
-        style={{ top: 70 }}
-      >
-        <div className="category_setting_columns">
-          <ul>
-            <li>추가</li>
-            <li>카테고리 명</li>
-            <li>이름변경</li>
-            <li>표시순서<br/>변경</li>
-            <li>삭제</li>
-            <li>총 책권수</li>
-            <li>책 제목모음</li>
-          </ul>
-        </div>
-        {categoryList}
-      </Modal>
-    </>
-  );
+
+class CategoryModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      visible: false
+     };
+  }
+
+  render() {
+    const categoryList = this.props.category.map((category)=>(
+      <CategoryList changeCategoryHandler={this.props.changeCategoryHandler} addCategory={this.props.addCategory} key={category._id} category={category}/>
+    ))
+    
+    return (
+      <>
+        <SettingOutlined  onClick={() => this.setState({visible:true})} style={{fontSize:'14px'}}/>
+        <Modal
+          title={[<SettingOutlined  onClick={() => this.setState({visible:true})} style={{fontSize:'14px'}}/>,<span style={{fontSize:"12px"}}> 카테고리 설정</span>]}
+          visible={this.state.visible}
+          onCancel={() => this.setState({visible:false})}
+          width={1000}
+          footer={null}
+          style={{ top: 70 }}
+        >
+          <div className="category_setting_columns">
+            <ul>
+              <li>추가</li>
+              <li>카테고리 명</li>
+              <li>이름변경</li>
+              <li>표시순서<br/>변경</li>
+              <li>삭제</li>
+              <li>총 책권수</li>
+              <li>책 제목모음</li>
+            </ul>
+          </div>
+          {categoryList}
+        </Modal>
+      </>
+    )
+  }
 }
 
 export default CategoryModal
--- client/src/components/views/Write/ChangeCategoryName.js
+++ client/src/components/views/Write/ChangeCategoryName.js
@@ -1,29 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Form, Input, Button, Space } from 'antd';
 
 const ChangeCategory = (props) => {
   const [form] = Form.useForm();
-  console.log('visible :',props.visible)
-  const [visible, SetVisible] = useState(true)
   const onFinish = value => {
     props.changeCategoryHandler({value, categoryId:props.category._id})
-    props.onClick()
+    props.inputAreaVisible()
   };
   const cancel = () => {
-    props.onClick()
+    props.inputAreaVisible()
+    console.log(props.inputAreaVisible)
     console.log('cancel clicked!!!')
+    console.log(props.vi)
   }
-  const visibleToggle = () =>{
-    SetVisible({visible:false});
-    console.log(visible)
-  }
+
   return (
     <>
       <Form
         layout={'inline'}
         form={form}
         size="small"
-        visible={false}
         onFinish={onFinish}
         className="change_book_title_input_form"
       >
@@ -33,7 +29,7 @@ const ChangeCategory = (props) => {
         </Form.Item>
         <Form.Item className="change_book_title_buttons">
           <Button type="primary" htmlType="submit">완료</Button>
-          <Button type="primary" onClick={visibleToggle}>취소</Button>
+          <Button type="primary" onClick={cancel}>취소</Button>
         </Form.Item>
         </Space>
       </Form>
