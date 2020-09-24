286efbd9cd0379aca524bc73c6fd3ecfd80407b8
kizwond <kizwond@gmail.com>
2020-09-24 10:50:56 +0900

    froala editer added

--- client/src/components/views/Write/BookEditing/BookWriting.js
+++ client/src/components/views/Write/BookEditing/BookWriting.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
 var userId = localStorage.getItem('userId')
 
 export class BookWriting extends Component {
@@ -10,11 +16,18 @@ export class BookWriting extends Component {
        bookTitle:'',
        category:'',
        userEmail:'',
-       user : ''
-
+       user : '',
+       model: 'Example text'
     }
+    this.handleModelChange = this.handleModelChange.bind(this);
   }
-  
+
+  handleModelChange(model) {
+    this.setState({
+      model: model
+    });
+  }
+
   componentDidMount() {
     this.setState({
       user: userId
@@ -33,12 +46,34 @@ export class BookWriting extends Component {
       })
   }
   render() {
+    const config={
+  placeholder: "Edit Me",
+  events : {
+    'focus' : function(e, editor) {
+      console.log(editor.selection.get());
+    }
+  }
+}
     return (
+      <>
       <div style={{width:"200px", margin:"auto", marginTop:"100px"}}>
         <div>작성자 : {this.state.userEmail}</div>
         <div>분류 : {this.state.category}</div>
         <div>제목 : {this.state.bookTitle}</div>
       </div>
+      <div  id="editor">
+      <FroalaEditorComponent
+          tag='textarea'
+          config={this.config}
+          model={this.state.model}
+          onModelChange={this.handleModelChange}
+        />
+        <textarea value={this.state.model}/>
+        <FroalaEditorView
+            model={this.state.model}
+          />
+      </div>
+      </>
     )
   }
 }
