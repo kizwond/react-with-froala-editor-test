3d77bdcf312be43da898a2dd561e8ea447cf14c1
kizwond <kizwond@gmail.com>
2020-09-28 12:18:14 +0900

    bookwritingmain start

--- client/src/components/views/Write/BookEditing/BookWriting.js
+++ client/src/components/views/Write/BookEditing/BookWriting.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-import 'froala-editor/js/froala_editor.pkgd.min.js'
-import 'froala-editor/css/froala_style.min.css'
-import 'froala-editor/css/froala_editor.pkgd.min.css'
-import 'froala-editor/css/plugins.pkgd.min.css'
-import 'froala-editor/js/plugins.pkgd.min.js'
-import 'froala-editor/js/languages/ko'
-import 'froala-editor//css/themes/gray.min.css'
-
-
-import FroalaEditorComponent from 'react-froala-wysiwyg';
-import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
 var userId = localStorage.getItem('userId')
 
 export class BookWriting extends Component {
@@ -45,7 +34,7 @@ export class BookWriting extends Component {
     
     return (
       <>
-      <div style={{width:"200px", margin:"auto", marginTop:"100px"}}>
+      <div>
         <div>작성자 : {this.state.userEmail}</div>
         <div>분류 : {this.state.category}</div>
         <div>제목 : {this.state.bookTitle}</div>
