5bc820f1551d6424b29db4cd58b22f171a52f1d4
kizwond <kizwond@gmail.com>
2020-10-08 19:23:40 +0900

    contentstable route updated

--- client/src/components/views/Write/BookEditing/ContentsTable.js
+++ client/src/components/views/Write/BookEditing/ContentsTable.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { Modal } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Modal, Popover,Form, Input, Button, Space } from 'antd';
 import { PlusCircleOutlined,DeleteOutlined,CaretDownOutlined,CaretUpOutlined,SettingOutlined,EditOutlined,StepBackwardOutlined,StepForwardOutlined } from '@ant-design/icons';
 import './ContentsTable.css'
 class ContentsTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      
+     };
   }
   render() {
     console.log(this.props.table_of_contents)
@@ -47,14 +49,58 @@ class ContentsTable extends Component {
 class ContentsTableList extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      inputArea : false,
+      newInput : false
+     };
   }
+
+  onFinish = value => {
+    this.props.addTable({value, prevTableId:this.props.table_of_contents._id})
+    this.newInputVisible()
+  };
+
+  inputAreaVisible = () =>{
+    this.setState(state => ({
+      inputArea: !state.inputArea
+    }));
+  }
+  newInputVisible = () =>{
+    this.setState(state => ({
+      newInput: !state.newInput
+    }));
+  }
+
   render() {
-    const contentsTableList = this.props.table_of_contents.map((content)=>(
-      <>
+    const text = <span>새로운 목차를 입력해 주세요.</span>;
+    const content = (
+      <Form
+          layout={'inline'}
+          size="small"
+          onFinish={this.onFinish}
+          className="change_book_title_input_form"
+        >
+          <Space>
+          <Form.Item name={['newCategory']} rules={[{ required: true }]} >
+            <Input placeholder='' />
+          </Form.Item>
+          <Form.Item className="change_book_title_buttons">
+            <Button type="primary" htmlType="submit">완료</Button>
+            <Button type="primary" onClick={this.newInputVisible}>취소</Button>
+          </Form.Item>
+          </Space>
+        </Form>
+    );
+
+    const contentsTableList = this.props.table_of_contents.map((table)=>(
+      < Fragment key={content._id}>
         <div className="mokcha_levels">
           <div></div>
-          <div><PlusCircleOutlined /> {content.table_name}</div>
+          <div>
+          <Popover placement="rightTop" editCategory title={text} visible={this.state.newInput} content={content} trigger="click">
+            <PlusCircleOutlined onClick={this.newInputVisible} style={{fontSize:'14px'}} /> 
+          </Popover>
+            {table.table_name}</div>
           <div></div>
           <div></div>
           <div></div>
@@ -66,7 +112,7 @@ class ContentsTableList extends Component {
           <div><CaretUpOutlined /> <CaretDownOutlined /></div>
           <div><DeleteOutlined /></div>
         </div>
-    </>
+      </Fragment>
     ))
     return (
       <>
