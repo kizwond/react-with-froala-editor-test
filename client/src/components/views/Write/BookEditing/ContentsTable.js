45d35dc4044e3051471f7986dab49abab8a48465
kizwond <kizwond@gmail.com>
2020-10-08 13:42:13 +0900

    contentsTable start

--- client/src/components/views/Write/BookEditing/ContentsTable.js
+++ client/src/components/views/Write/BookEditing/ContentsTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Modal } from 'antd';
-import { SettingOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined,DeleteOutlined,CaretDownOutlined,CaretUpOutlined,SettingOutlined,EditOutlined,StepBackwardOutlined,StepForwardOutlined } from '@ant-design/icons';
+import './ContentsTable.css'
 class ContentsTable extends Component {
   constructor(props) {
     super(props);
@@ -14,10 +15,42 @@ class ContentsTable extends Component {
         onOk={this.props.handleOk}
         onCancel={this.props.handleCancel}
         footer={null}
+        width={900}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div className="mokcha_container">
+          <div className="mokcha_columns">
+            <div className="mokcha_left_columns">
+              <div>레벨</div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+            </div>
+            <div className="mokcha_right_columns">
+              <div>이름변경</div>
+              <div>레벨이동</div>
+              <div>순서이동</div>
+              <div>삭제</div>
+            </div>
+          </div>
+          <div className="mokcha_contents">
+            <div className="mokcha_levels">
+              <div></div>
+              <div><PlusCircleOutlined /></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="mokcha_tools">
+              <div><EditOutlined /></div>
+              <div><StepBackwardOutlined /> <StepForwardOutlined /></div>
+              <div><CaretUpOutlined /> <CaretDownOutlined /></div>
+              <div><DeleteOutlined /></div>
+            </div>
+          </div>
+        </div>
       </Modal>
     );
   }
