e6d6400e9d0babc67dad6d057486d98c93280378
kizwond <kizwond@gmail.com>
2020-10-14 11:11:30 +0900

    treeData custmize attempt

--- client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
+++ client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Tabs } from 'antd';
 import { Button, Modal, Tree } from 'antd';
-import { UnorderedListOutlined, DoubleLeftOutlined  } from '@ant-design/icons';
+import { UnorderedListOutlined, DoubleLeftOutlined,CarryOutOutlined, FormOutlined  } from '@ant-design/icons';
 import ContentsTable from './ContentsTable'
 const { TabPane } = Tabs;
+
 
 class LeftDrawer extends Component {
   constructor(props) {
@@ -46,9 +47,51 @@ class LeftDrawer extends Component {
     } else {
       var toggle = <DoubleLeftOutlined />
     }
-    const contentsTableList = this.props.table_of_contents.map((table)=>(
-      <div>{table.table_name}</div>
-    ))
+    const contentsTableList = this.props.table_of_contents.map((table)=>{
+        if(table.level === 1){
+          let object = {
+            title: table.table_name,
+            key: table.order,
+            icon: <CarryOutOutlined />,
+            children: [],}
+          return object
+        } else if (table.level === 2){
+          let object = {
+            title: table.table_name,
+            key: table.order,
+            icon: <CarryOutOutlined />,
+            children: [],}
+          return object
+        }else if (table.level === 3){
+          let object = {
+            title: table.table_name,
+            key: table.order,
+            icon: <CarryOutOutlined />,
+            children: [],}
+          return object
+        }else if (table.level === 4){
+          let object = {
+            title: table.table_name,
+            key: table.order,
+            icon: <CarryOutOutlined />,
+            children: [],}
+          return object
+        }else if (table.level === 5){
+          let object = {
+            title: table.table_name,
+            key: table.order,
+            icon: <CarryOutOutlined />,
+            children: [],}
+          return object
+        }
+      }
+    )
+    console.log(contentsTableList)
+    const treeData = contentsTableList
+    
+    const onSelect = (selectedKeys, info) => {
+      console.log('selected', selectedKeys, info);
+    };
     return (
         <Tabs defaultActiveKey={this.state.order_key} className="left_drawer" onChange={this.handleChange} type="card" size='small' tabPosition={mode} >
           <TabPane tab={toggle} key="none">
@@ -57,7 +100,13 @@ class LeftDrawer extends Component {
             <div><Button onClick={this.showModal} size="small">목차편집</Button> </div>
             <ContentsTable tableDeleteHandler={this.props.tableDeleteHandler} tableOrderlHandler={this.props.tableOrderlHandler} tableLevelHandler={this.props.tableLevelHandler} changeTableNameHandler={this.props.changeTableNameHandler}  addTable={this.props.addTable} table_of_contents={this.props.table_of_contents} handleOk={this.handleOk} showModal={this.showModal} handleCancel={this.handleCancel} visible={this.state.visible}/>
             <div className="table_of_contents_container">
-              {contentsTableList}
+              <Tree
+                showLine={true}
+                showIcon={true}
+                defaultExpandedKeys={['0-0-0']}
+                onSelect={onSelect}
+                treeData={treeData}
+              />
             </div>
           </TabPane>
         </Tabs>
