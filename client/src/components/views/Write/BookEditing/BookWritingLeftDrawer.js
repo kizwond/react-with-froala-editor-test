02f27960fbf7a9e6a9ceb9910c879bd9cfbb5055
kizwond <kizwond@gmail.com>
2020-10-15 13:57:25 +0900

    treedata working for now

--- client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
+++ client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
@@ -99,10 +99,15 @@ class LeftDrawer extends Component {
     )
 
     const level_5 = obj => obj.level === 5;
+    const level_4 = obj => obj.level === 4;
+    const level_3 = obj => obj.level === 3;
+    const level_2 = obj => obj.level === 2;
     if(level_all.length > 0){
-      let exist = level_all.some(level_5)
-      if(exist === true){
-
+      let level_5_exist = level_all.some(level_5)
+      let level_4_exist = level_all.some(level_4)
+      let level_3_exist = level_all.some(level_3)
+      let level_2_exist = level_all.some(level_2)
+      if(level_5_exist === true){
         let temp_data_4 = []
         
         for(var i = 0; i < level_all.length; i += 1) {
@@ -195,6 +200,154 @@ class LeftDrawer extends Component {
             }
           }
     
+          console.log('result:',level_all)
+    
+        }
+      } else if(level_4_exist === true){
+        let temp_data_3 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 3) {
+              temp_data_3.push(level_all[i])
+            } else if(level_all[i]['level'] === 4) {
+              for(var a = 0; a < temp_data_3.length; a += 1) {
+                temp_data_3[temp_data_3.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+        
+        if(temp_data_3.length > 0){
+          var i = 0;
+          while (i < level_all.length) {
+            if (level_all[i]['level'] === 4) {
+              level_all.splice(i, 1);
+            } else {
+              ++i;
+            }
+          }
+
+          let temp_data_2 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 2) {
+              temp_data_2.push(level_all[i])
+            } else if(level_all[i]['level'] === 3) {
+              for(var a = 0; a < temp_data_2.length; a += 1) {
+                temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_2.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 3) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+    
+          let temp_data_1 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 1) {
+              temp_data_1.push(level_all[i])
+            } else if(level_all[i]['level'] === 2) {
+              for(var a = 0; a < temp_data_1.length; a += 1) {
+                temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_1.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 2) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+    
+          console.log('result:',level_all)
+    
+        }
+      } else if(level_3_exist === true){
+        let temp_data_2 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 2) {
+              temp_data_2.push(level_all[i])
+            } else if(level_all[i]['level'] === 3) {
+              for(var a = 0; a < temp_data_2.length; a += 1) {
+                temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+        
+        if(temp_data_2.length > 0){
+          var i = 0;
+          while (i < level_all.length) {
+            if (level_all[i]['level'] === 3) {
+              level_all.splice(i, 1);
+            } else {
+              ++i;
+            }
+          }
+    
+          let temp_data_1 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 1) {
+              temp_data_1.push(level_all[i])
+            } else if(level_all[i]['level'] === 2) {
+              for(var a = 0; a < temp_data_1.length; a += 1) {
+                temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_1.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 2) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+    
+          console.log('result:',level_all)
+    
+        }
+      } else if(level_2_exist === true){
+
+        let temp_data_1 = []
+        for(var i = 0; i < level_all.length; i += 1) {
+          if(level_all[i]['level'] === 1) {
+            temp_data_1.push(level_all[i])
+          } else if(level_all[i]['level'] === 2) {
+            for(var a = 0; a < temp_data_1.length; a += 1) {
+              temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+              break;
+            }
+          } 
+        }
+
+        if(temp_data_1.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 2) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          
           console.log('result:',level_all)
     
         }
