3dc33fb6d5da657187e33acf1ceed15487cde3a4
kizwond <kizwond@gmail.com>
2020-10-16 15:03:49 +0900

    error fixing.....

--- client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
+++ client/src/components/views/Write/BookEditing/BookWritingLeftDrawer.js
@@ -107,30 +107,58 @@ class LeftDrawer extends Component {
       let level_4_exist = level_all.some(level_4)
       let level_3_exist = level_all.some(level_3)
       let level_2_exist = level_all.some(level_2)
+
+      //level_5 exist
       if(level_5_exist === true){
         let temp_data_4 = []
-        
+        let temp_data_3 = []
+        let temp_data_2 = []
+        let temp_data_1 = []
+
         for(var i = 0; i < level_all.length; i += 1) {
           if(level_all[i]['level'] === 4) {
             temp_data_4.push(level_all[i])
-          } else if(level_all[i]['level'] === 5) {
+          } else if(level_all[i]['level'] === 5 && temp_data_4.length > 0) {
             for(var a = 0; a < temp_data_4.length; a += 1) {
               temp_data_4[temp_data_4.length - 1]['children'].push(level_all[i])
               break;
             }
-          } 
-        }
-        
-        if(temp_data_4.length > 0){
-          var i = 0;
-          while (i < level_all.length) {
-            if (level_all[i]['level'] === 5) {
-              level_all.splice(i, 1);
-            } else {
-              ++i;
+          } else if(level_all[i]['level'] === 3){
+            temp_data_3.push(level_all[i])
+            console.log(temp_data_3)
+          } else if(level_all[i]['level'] === 5 && temp_data_3.length > 0) {
+            for(var a = 0; a < temp_data_3.length; a += 1) {
+              temp_data_3[temp_data_3.length - 1]['children'].push(level_all[i])
+              break;
+            }
+          } else if(level_all[i]['level'] === 2){
+            temp_data_2.push(level_all[i])
+            console.log(temp_data_2)
+          } else if(level_all[i]['level'] === 5 && temp_data_2.length > 0) {
+            for(var a = 0; a < temp_data_2.length; a += 1) {
+              temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+              break;
+            }
+          } else if(level_all[i]['level'] === 1){
+            temp_data_1.push(level_all[i])
+            console.log(temp_data_1)
+          } else if(level_all[i]['level'] === 5 && temp_data_1.length > 0) {
+            for(var a = 0; a < temp_data_1.length; a += 1) {
+              temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+              break;
             }
           }
-          
+        }
+        var i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 5) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+
+        if(temp_data_4.length > 0){
           let temp_data_3 = []
           for(var i = 0; i < level_all.length; i += 1) {
             if(level_all[i]['level'] === 3) {
@@ -199,10 +227,102 @@ class LeftDrawer extends Component {
               }
             }
           }
-    
-          console.log('result:',level_all)
-    
         }
+
+        if(temp_data_3.length > 0){
+          var i = 0;
+          while (i < level_all.length) {
+            if (level_all[i]['level'] === 4) {
+              level_all.splice(i, 1);
+            } else {
+              ++i;
+            }
+          }
+
+          let temp_data_2 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 2) {
+              temp_data_2.push(level_all[i])
+            } else if(level_all[i]['level'] === 3) {
+              for(var a = 0; a < temp_data_2.length; a += 1) {
+                temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_2.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 3) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+    
+          let temp_data_1 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 1) {
+              temp_data_1.push(level_all[i])
+            } else if(level_all[i]['level'] === 2) {
+              for(var a = 0; a < temp_data_1.length; a += 1) {
+                temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_1.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 2) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+        }
+
+        if(temp_data_2.length > 0){
+          var i = 0;
+          while (i < level_all.length) {
+            if (level_all[i]['level'] === 3) {
+              level_all.splice(i, 1);
+            } else {
+              ++i;
+            }
+          }
+    
+          let temp_data_1 = []
+          for(var i = 0; i < level_all.length; i += 1) {
+            if(level_all[i]['level'] === 1) {
+              temp_data_1.push(level_all[i])
+            } else if(level_all[i]['level'] === 2) {
+              for(var a = 0; a < temp_data_1.length; a += 1) {
+                temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+                break;
+              }
+            } 
+          }
+          
+          if(temp_data_1.length > 0){
+            var i = 0;
+            while (i < level_all.length) {
+              if (level_all[i]['level'] === 2) {
+                level_all.splice(i, 1);
+              } else {
+                ++i;
+              }
+            }
+          }
+    
+          
+    
+        }console.log('result:',level_all)
+        //level_4 exist
       } else if(level_4_exist === true){
         let temp_data_3 = []
           for(var i = 0; i < level_all.length; i += 1) {
@@ -272,9 +392,10 @@ class LeftDrawer extends Component {
             }
           }
     
-          console.log('result:',level_all)
+          
     
-        }
+        }console.log('result:',level_all)
+        //level_3 exist
       } else if(level_3_exist === true){
         let temp_data_2 = []
           for(var i = 0; i < level_all.length; i += 1) {
@@ -321,9 +442,10 @@ class LeftDrawer extends Component {
             }
           }
     
-          console.log('result:',level_all)
+          
     
-        }
+        }console.log('result:',level_all)
+        //level_2 exist
       } else if(level_2_exist === true){
 
         let temp_data_1 = []
